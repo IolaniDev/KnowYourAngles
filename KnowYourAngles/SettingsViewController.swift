18b07cdee1d31344231ca3679a4061d09e16e538
Cart 115 Administrator <admin@cart115-lap02.local>
2016-07-07 12:32:22 -1000

    added settingsviewcontroller and started implementing switches and sliders.

--- KnowYourAngles/SettingsViewController.swift
+++ KnowYourAngles/SettingsViewController.swift
@@ -10,4 +10,40 @@ import Foundation
 
 public class SettingsViewController: UIViewController{
     
+    @IBOutlet weak var totalNumOfProblems: UILabel!
+    @IBOutlet weak var timerSlider: UISlider!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    override public func viewDidLoad() {
+        totalNumOfProblems.text = "Number of Problems: 10";
+        timerSlider.hidden = false;
+        timerLabel.text = "Timer: 30 seconds";
+    }
+    
+    @IBAction func sliderChanged(sender: UISlider) {
+        totalNumOfProblems.text = "Number of Problems: \(lroundf(sender.value))";
+    }
+    
+    @IBAction func timerSwitchChanged(sender: UISwitch) {
+        if(sender.on){
+            timerSlider.hidden = false;
+        }
+        else
+        {
+            timerSlider.hidden = true;
+        }
+    }
+    
+    @IBAction func timerSliderChanged(sender: UISlider) {
+      
+        if(sender.value<1)
+        {
+            timerLabel.text = "Timer: 30 seconds";
+        }
+        else
+        {
+            let newValue = lroundf(((sender.value - 0.25)/0.5)*0.5)
+            timerLabel.text = "Timer: \(newValue) minutes";
+        }
+    }
 }
--- KnowYourAngles/ViewController.swift
+++ KnowYourAngles/ViewController.swift
@@ -13,11 +13,22 @@ class ViewController: UIViewController, MAWMathViewDelegate{
     
     //totalNumOfProblems is set in Settings by user for the number of problems they want to be quizzed on.
     
+    //possible problems
+    var degreeProblems: [String] = ["Cos0", "Cos30", "Cos45", "Cos60", "Cos90", "Cos120", "Cos135", "Cos150", "Cos180", "Cos210", "Cos225", "Cos240", "Cos270", "Cos300", "Cos315", "Cos330", "Sin0", "Sin30", "Sin45", "Sin60", "Sin90", "Sin120", "Sin135", "Sin150", "Sin180", "Sin210", "Sin225", "Sin240", "Sin270", "Sin300", "Sin315", "Sin330", "tan0", "tan30", "tan45", "tan60", "tan90", "tan120", "tan135", "tan150", "tan180", "tan210", "tan225", "tan240", "tan270", "tan300", "tan315", "tan330"];
+    
+    var radianProblems = [String]();
+    
+    var reciprocalProblems = [String]();
+    
+    //image of current problem.
+    @IBOutlet weak var problemImage: UIImageView!
+    
     //mathView holds the view where you can write answers
     @IBOutlet var mathView: MAWMathView!
     var certificateRegistered : Bool!;
     
     override func viewDidLoad() {
+        //Setup Writing Recognition
         super.viewDidLoad();
         
         // Register MyScript certificate before anything else
@@ -43,7 +54,9 @@ class ViewController: UIViewController, MAWMathViewDelegate{
             
             mathView.configureWithBundle("math", andConfig: "standard");
             
-            
+            //setup image of first problem
+            let randomNum = Int(arc4random_uniform(UInt32(degreeProblems.count)));
+            problemImage.image = UIImage(named: degreeProblems[randomNum]);
         }
     }
     
