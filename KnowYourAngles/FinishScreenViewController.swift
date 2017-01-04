0712b7a984fe09721c498d78e2b8571eaa529421
iPad App Dev <appdev@us-scil-appdev-01.local>
2017-01-04 13:40:38 -1000

    cleaned up the finish screen view controller

--- KnowYourAngles/FinishScreenViewController.swift
+++ KnowYourAngles/FinishScreenViewController.swift
@@ -17,33 +17,35 @@ public class FinishScreenViewController: UIViewController{
     var finalTime = (0,0);
     var isTimerOn = false;
     
-    //references to UI components on storyboard
+    // references to UI components on storyboard
     @IBOutlet weak var finishScore: UILabel!
     @IBOutlet weak var finishTime: UILabel!
     
     // load previously saved settings (if there are any)
-    let savedSettings = NSUserDefaults.standardUserDefaults()
+    private let savedSettings = NSUserDefaults.standardUserDefaults()
     
-    //view did load
+    // view did load
     override public func viewDidLoad() {
-        //set the score results
+        // set the score results
         finishScore.text = "Score: \(finalScore) out of \(totalNum)";
-        //if the timer was on, then output the time it took
+        
+        // if the timer was on, then output the time it took to answer all of the questions
         if (isTimerOn)
         {
-            //output the time used to answer the questions
+            // output the time used to answer the questions
             finishTime.text = String(format: "Time: %02d:%02d", finalTime.0, finalTime.1);
         }
-        //otherwise don't display anything about the time
+        // otherwise don't display anything about the time
         else
         {
             finishTime.text = "";
         }
-        //log results in high scores
+        
+        // log results in high scores
         logResultsInHighScores();
     }
     
-    //added functionality to a button so the results can be saved as a picture for sharing
+    // added functionality to a button component so the results after answering questions can be saved as a picture for sharing
     @IBAction func saveScreenshot(sender: UIButton) {
         UIGraphicsBeginImageContext(view.frame.size);
         view.layer.renderInContext(UIGraphicsGetCurrentContext()!);
@@ -52,12 +54,13 @@ public class FinishScreenViewController: UIViewController{
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
     }
     
-    //function to save the results in the High Score tables
+    // function to save the results in the High Score tables
     func logResultsInHighScores()
     {
-        //get the managed object context
+        // get the managed object context
         let managedContext = DataController().managedObjectContext;
-        //create a fetch request for all Table entities
+        
+        // create a fetch request for all Table entities
         let fetchRequest = NSFetchRequest(entityName: "Table");
         
         //create the new High Score entry
@@ -69,103 +72,66 @@ public class FinishScreenViewController: UIViewController{
         score.setValue(finalTime.1, forKey: "timeTakenSec")
         score.setValue(totalNum, forKey: "totalProblems")
         
-        //set the category for the new score entry
+        // set the category for the new score entry
         var categories = "";
+        // include deg if the set of problems had degrees in them
         if(savedSettings.valueForKey("degrees") as! Bool)
         {
             categories += "deg"
         }
+        // include rad if the set of problems had radians
         if(savedSettings.valueForKey("radians") as! Bool)
         {
             categories += "rad"
         }
+        // include rec if the set of problems included reciprocals
         if(savedSettings.valueForKey("reciprocals") as! Bool)
         {
             categories += "rec"
         }
         
+        // set the category for the High Score entry
         score.setValue(categories, forKey: "category");
         
         //if there was no time limit
         if(!isTimerOn)
         {
             categories = "highScores0"+categories;
-            //modify the fetch request to only include the Table with category highScores0
-            /*fetchRequest.predicate = NSPredicate(format: "category = %@", "highScores0"+categories)
-            
-            do{
-                //run the fetch request and store the resulting Table in results
-                let results = try managedContext.executeFetchRequest(fetchRequest);
-                
-                //if the table does not exist...
-                if(results.count == 0)
-                {
-                    //create a table
-                    let newTable = NSEntityDescription.insertNewObjectForEntityForName("Table", inManagedObjectContext: managedContext);
-                    newTable.setValue("highScores0"+categories, forKey: "category");
-                    (score as! HighScore).table = (newTable as! Table)
-                }
-                //if the table already exists...
-                else{
-                    //get the table
-                    let tempTable = results[0] as! Table
-                    // add the new High Score into the records
-                    (score as! HighScore).table = tempTable
-                    if(tempTable.highScores?.count>5)
-                    {
-                        managedContext.deleteObject(tempTable.highScores?.reversedOrderedSet.lastObject as! HighScore);
-                    }
-                    
-                }
-            }catch let error as NSError{
-                print("Could not fetch \(error), \(error.userInfo)")
-            }
-            catch let e as NSException{
-                print("\(e.userInfo)");
-            }
-            catch{
-                print("You done goofed.")
-            }
-            
-            do{
-                try managedContext.save();
-                
-            }catch let error as NSError{
-                print("Could not save \(error), \(error.userInfo)")
-            }*/
         }
         //if the timer was on, then figure out how much time was allotted.
         else
         {
-            //get the time allotted from saved settings
-            //let savedSettings = NSUserDefaults.standardUserDefaults();
+            //get the time allowed from saved settings
             let timeAllowed = (savedSettings.integerForKey("amtTimeMin"), savedSettings.integerForKey("amtTimeSec"));
             
             //if the time allowed was 30 seconds...
             if(timeAllowed.0 == 0 && timeAllowed.1 == 30)
             {
                 categories = "highScores30" + categories;
-
             }
-            //else if the time allowed was 1 minute
-            else if(timeAllowed.0 == 1 && timeAllowed.1 == 0)
+            //else if the time allowed was 1 minute or 1 minute 30 seconds
+            else if(timeAllowed.0 == 1)
             {
-                categories = "highScores1" + categories;
+                if(timeAllowed.1 == 0)
+                {
+                    categories = "highScores1" + categories;
+                }
+                else // if(timeAllowed.1 == 30)
+                {
+                    categories = "highScores130" + categories;
+                }
             }
-            //else if the time allowed was 1 minute 30 seconds
-            else if(timeAllowed.0 == 1 && timeAllowed.1 == 30)
-            {
-                categories = "highScores130" + categories;
-            }
-            //else if the time allowed was 2 minutes
+            //else if the time allowed was 2 minutes or 2 minutes 30 seconds
             else if(timeAllowed.0 == 2 && timeAllowed.1 == 0)
             {
-                categories = "highScores2" + categories;
-            }
-            //else if the time allowed was 2 minutes 30 seconds
-            else if(timeAllowed.0 == 2 && timeAllowed.1 == 30)
-            {
-                categories = "highScores230" + categories;
+                if(timeAllowed.1 == 0)
+                {
+                    categories = "highScores2" + categories;
+                }
+                else // if(timeAllowed.1 == 30)
+                {
+                    categories = "highScores230" + categories;
+                }
             }
             //else if the time allowed was 3 minutes
             else
@@ -188,7 +154,7 @@ public class FinishScreenViewController: UIViewController{
                 newTable.setValue(categories, forKey: "category");
                 (score as! HighScore).table = (newTable as! Table)
             }
-                // if the table already exists...
+            // if the table already exists...
             else{
                 // get the table
                 let tempTable = results[0] as! Table
@@ -206,7 +172,7 @@ public class FinishScreenViewController: UIViewController{
             print("\(e.userInfo)");
         }
         catch{
-            print("You done goofed.")
+            print("You messed up.")
         }
         
         do{
@@ -216,24 +182,4 @@ public class FinishScreenViewController: UIViewController{
             print("Could not save \(error), \(error.userInfo)")
         }
     }
-    
-    /*func saveHighScores(forKey: String, scores: [NSManagedObject])
-    {
-        //let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
-     
-        let managedContext = DataController().managedObjectContext;
-     
-        let entity = NSEntityDescription.entityForName("Table", inManagedObjectContext: managedContext)
-        
-        let temp = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
-        
-        temp.setValue(forKey, forKey: "category");
-        
-        do{
-            try managedContext.save();
-            
-        }catch let error as NSError{
-            print("Could not save \(error), \(error.userInfo)")
-        }
-    }*/
 }
