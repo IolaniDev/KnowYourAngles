bf10e12faef8bf206ed3ced1118f7cf118d4f4a7
enagoshi <enagoshi@iolani.org>
2018-06-25 17:40:57 -1000

    removed Crashlytics from app moved some fonts from ATKMathWidget.bundle to main project folder so that the fonts could load without error

--- KnowYourAngles/AppDelegate.swift
+++ KnowYourAngles/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Fabric
-import Crashlytics
 import CoreData
 
 @UIApplicationMain
@@ -18,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        Fabric.with([Crashlytics.self])
         return true
     }
 
