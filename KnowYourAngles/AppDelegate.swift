a0478c5af059f9a1dc5e7e88ea91050a63eb8252
Erin Nagoshi <enagoshi@iolani.org>
2021-07-18 20:39:19 -1000

    Started converting to SwiftUI by disabling the AppDelegate as the "main", creating a Swift file called KnowYourAnglesApp.swift which will serve as the entry point for the app, and created a template SwiftUI View file which will eventually hold the main menu.

--- KnowYourAngles/AppDelegate.swift
+++ KnowYourAngles/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 //import Firebase
 
-@UIApplicationMain
+//@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
---
+++ KnowYourAngles/KnowYourAnglesApp.swift
@@ -0,0 +1,20 @@
+//
+//  KnowYourAnglesApp.swift
+//  KnowYourAngles
+//
+//  Created by Erin Nagoshi on 7/18/21.
+//  Copyright © 2021 Iolani School. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+//@main identifies the app's entry point (i.e. first scene)
+
+struct KnowYourAnglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
