00b972bf19453dde3c7eeec6213ff0778596c12a
Erin Nagoshi <enagoshi@iolani.org>
2021-07-18 20:39:19 -1000

    Started converting to SwiftUI by disabling the AppDelegate as the "main", changing iOS version to 14.1, creating a Swift file called KnowYourAnglesApp.swift which will serve as the entry point for the app, and created a template SwiftUI View file which will eventually hold the main menu.

---
+++ KnowYourAngles/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  KnowYourAngles
+//
+//  Created by Erin Nagoshi on 7/18/21.
+//  Copyright © 2021 Iolani School. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
