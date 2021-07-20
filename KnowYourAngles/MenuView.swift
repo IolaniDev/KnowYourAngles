438ec34b72a6c6b1f51f698b124ce5208167380d
Erin Nagoshi <enagoshi@iolani.org>
2021-07-19 14:15:46 -1000

    Created new icons for Main Menu, added MenuView

--- KnowYourAngles/MenuView.swift
+++ KnowYourAngles/MenuView.swift
@@ -10,7 +10,37 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Color(red: 25.0/255, green: 127.0/255, blue: 124.0/255)
+            .edgesIgnoringSafeArea(.all)
+            .overlay(
+                VStack{
+                    Text("Know Your Angles")
+                        .font(.system(size: 80, weight: .heavy, design: .serif))
+                        .foregroundColor(Color(red: 127.0/255, green: 255.0/255, blue: 250.0/255, opacity: 1.0))
+                    HStack{
+                        Image("KYA_Start_Icon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Image("KYA_Settings_Icon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .frame(width: 800, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    HStack{
+                        Image("KYA_Statistics_Icon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Image("KYA_About_Icon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .frame(width: 800, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+                .padding()
+                .frame(minWidth: 800, maxWidth: .infinity, minHeight: 1000, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                //.listRowInsets(EdgeInsets())
+                
+            )
     }
 }
 
