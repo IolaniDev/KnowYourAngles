37f0993639261ed478a15cc3993a0c9bf509fcdb
Erin Nagoshi <enagoshi@iolani.org>
2021-07-20 15:17:01 -1000

    Added a way to navigate to the "About" page (which is displayed as a popover screen that can be dismissed). Also added an "About" page and made the "AboutViewController.swift" file obsolete.

--- KnowYourAngles/AboutViewController.swift
+++ KnowYourAngles/AboutViewController.swift
@@ -1,31 +1,31 @@
+////
+////  AboutViewController.swift
+////  KnowYourAngles
+////
+////  Created by iPad App Dev on 6/23/20.
+////  Copyright © 2020 Iolani School. All rights reserved.
+////
 //
-//  AboutViewController.swift
-//  KnowYourAngles
+//import Foundation
 //
-//  Created by iPad App Dev on 6/23/20.
-//  Copyright © 2020 Iolani School. All rights reserved.
+//extension UIApplication {
+//    static var appVersion: String? {
+//        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+//    }
+//}
 //
-
-import Foundation
-
-extension UIApplication {
-    static var appVersion: String? {
-        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
-    }
-}
-
-open class AboutViewController: UIViewController
-{
-    
-    @IBOutlet weak var versionLabel: UILabel!
- 
-    //when the view loads...
-       override open func viewDidLoad() {
-           //call on the super class viewDidLoad method
-           super.viewDidLoad()
-        //get the version number or set the string to 0.0.0 if there's some kind of error.
-        let versionNumber:String = UIApplication.appVersion ?? "0.0.0";
-        
-        versionLabel.text = "Version: " + versionNumber;
-    }
-}
+//open class AboutViewController: UIViewController
+//{
+//
+//    @IBOutlet weak var versionLabel: UILabel!
+//
+//    //when the view loads...
+//       override open func viewDidLoad() {
+//           //call on the super class viewDidLoad method
+//           super.viewDidLoad()
+//        //get the version number or set the string to 0.0.0 if there's some kind of error.
+//        let versionNumber:String = UIApplication.appVersion ?? "0.0.0";
+//
+//        versionLabel.text = "Version: " + versionNumber;
+//    }
+//}
