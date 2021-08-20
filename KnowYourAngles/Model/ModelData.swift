8b51f7242681a26ed7ef7aeca262e6b25d05fe03
Erin Nagoshi <enagoshi@iolani.org>
2021-08-19 21:47:16 -1000

    Cleaned up some unused files, deleted some commented-out code, and hopefully fixed an error where hitting submit right before the timer runs out causes the app to crash

--- KnowYourAngles/Model/ModelData.swift
+++ KnowYourAngles/Model/ModelData.swift
@@ -21,6 +21,7 @@ final class ModelData : ObservableObject {
     //finished indicates whether all of the problems selected have been answered
     @Published var finished = false
     var numberOfCorrectAnswers = 0
+    //hasStarted indicates whether the user has been presented any problems yet
     @Published var hasStarted = false
     
     //reload all Problems from the json file and reset other variables
